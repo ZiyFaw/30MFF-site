62571c050f75feb4dd3445f813042556d3a99819
Ziy <ziyad.zf5@gmail.com>
2022-02-16 00:14:31 +0400

    ripples, code cleaning

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var topnav = document.getElementById("topnav");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > 0) {
     topnav.classList.add("nav-solid-color")
@@ -13,12 +10,8 @@ function myFunction() {
   }
 }
 
-// if (document.body.scrollTop == 0) {
-//   // topnav.style.backgroundColor = "red";
-// }
-// topnav.style.backgroundColor = "red";
-
-// if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0)
-// {
-//     topnav.style.backgroundColor = "red";
-// }
+$('body').ripples({
+	resolution: 512,
+	dropRadius: 10,
+	perturbance: 0.04,
+});
